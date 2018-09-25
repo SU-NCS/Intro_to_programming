db7d5b78f4cd9a444a25214f222c92a33551c330
rebeccapeltz <rebeccapeltz@gmail.com>
2018-09-25 10:41:36 -0700

    upgrade per security

--- docs/docs/docs/docs/docs/docs/docs/docs/docs/docs/docs/computer-architecture.md
+++
@@ -1,6 +0,0 @@
-# Computer Architecture
-
-The diagram below shows the hardware of a computer and what the software has control over.
-
-![](assets/computer-architecture.png)
-
--- docs/gulpfile.js
+++ docs/gulpfile.js
@@ -1,43 +1,34 @@
 const gulp = require('gulp');
 const del = require('del');
 const exec = require('child_process').exec;
-var runSequence = require('run-sequence');
 const gulpLoadPlugins = require('gulp-load-plugins');
+
 
 const $ = gulpLoadPlugins();
 
-// Publishes the site to GitHub Pages
-// gulp.task('publish', () => {
-//   console.log('Publishing to GH Pages');
-//   return gulp.src('./_book/**/*')
-//     .pipe($.ghPages({
-//       origin: 'origin',
-//       branch: 'gh-pages'
-//     }));
-// });
+
 gulp.task('clean', function () {
   return del(['docs/**', 'docs/.*', '!docs'], {
     force: true
   });
 });
 gulp.task('copy', function () {
-  gulp.src([
-      '_book/**/*',
-      './*.md'
+  return gulp.src([
+      '_book/**/*'
     ])
     .pipe(gulp.dest('docs'));
 });
 
-gulp.task('build', function(){
+
+gulp.task('build', function (callback) {
+  // In gulp 4, you can return a child process to signal task completion
   exec('gitbook build', function (err, stdout, stderr) {
     console.log(stdout);
     console.log(stderr);
+    callback();
   });
 });
 
-
-gulp.task('default', function(callback) {
-  runSequence('clean',
-              'build',
-              'copy');
-});
+gulp.task('default', gulp.series('clean', 'build', 'copy', function (done) {
+  done();
+}));
